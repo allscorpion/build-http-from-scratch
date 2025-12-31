1a12e7321727df082b29e2c187d59eb6c5dda766
allscorpion <deadlyryz@hotmail.co.uk>
2025-12-31 19:41:46 +0000

    parsing headers

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Build HTTP from scratch
 
 A project to learn how to build http from scratch. 
+
+## Commands
+Use the following commands to test the tcp listener
+
+- go run ./cmd/tcplistener | tee {outputFileName}
+- curl http://localhost:42069/
+
+To test the udp listener you can use these commands 
+- go run ./cmd/udpsender
+- nc -u -l 42069
---
+++ internal/headers/headers.go
@@ -0,0 +1,62 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+type Headers map[string]string
+
+func (h Headers) Get(key string) string {
+	return h[strings.ToLower(key)]
+}
+
+func (h Headers) Set(key string, value string) {
+	h[strings.ToLower(key)] = value
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	newLineIndex := bytes.Index(data, []byte("\r\n"))
+
+	if newLineIndex == -1 {
+		return 0, false, nil
+	}
+
+	if newLineIndex == 0 {
+		return 2, true, nil
+	}
+
+	currentLine := strings.TrimSpace(string(data[:newLineIndex]))
+	keyColon := strings.Index(currentLine, ":")
+
+	if keyColon == -1 {
+		return 0, false, fmt.Errorf("malformed request-line: %v", currentLine)
+	}
+
+	key := currentLine[:keyColon]
+
+	if strings.Contains(key, " ") {
+		return 0, false, fmt.Errorf("spaces in request-line key: %v", key)
+	}
+
+	allowedSpecials := "!#$%&'*+-.^_`|~"
+	for _, r := range key {
+		if (r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') || (r >= '0' && r <= '9') {
+			continue
+		}
+		if strings.ContainsRune(allowedSpecials, r) {
+			continue
+		}
+		return 0, false, fmt.Errorf("invalid character %q in header key: %s", r, key)
+	}
+
+	value := strings.TrimSpace(currentLine[keyColon+1:])
+	h.Set(key, value)
+
+	return len(data[:newLineIndex]) + 2, false, nil
+}
+
+func NewHeaders() Headers {
+	return Headers{}
+}
